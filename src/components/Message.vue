8954be6ebe7914ca1c6291610bf132cfb0034194
Horiol <oriol.vall.hdez@gmail.com>
2020-11-08 18:34:26 +0100

    Add message vue component

---
+++ src/components/Message.vue
@@ -0,0 +1,49 @@
+<template>
+    <vs-alert shadow :color="color" flat style="margin:10px">
+        <template #title>
+            <div v-bind:class="{ my_message_title: isMine }">
+                <span v-if="isMine">You</span>
+                <span v-else>Other</span>
+            </div>
+        </template>
+            <div v-bind:class="{ my_message: isMine }">
+                {{message.msg}}
+            </div>
+    </vs-alert>
+</template>
+
+<script>
+export default {
+    name: 'Message',
+    props:{
+        message:Object
+    },
+    computed:{
+        isMine(){
+            if ('destiny' in this.message){
+                return true
+            } else {
+                return false
+            }
+        },
+        color: function(){
+            if ('destiny' in this.message){
+                return 'primary'
+            } else {
+                return "dark"
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.my_message{
+    text-align: end;
+}
+.my_message_title{
+    text-align: end;
+    display: block;
+    width: 100%;
+}
+</style>
